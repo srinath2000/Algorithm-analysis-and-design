27b284e9cc1cf1c64125ab3534b1bbf5d9384bfe
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2020-02-11 21:01:41 +0530

    Add files via upload
    comparison between the time complexities of array and linked list

---
+++ arr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+clock_t t1,t2;
+
+void main()
+{ int a[100000],n,i,m,lar,ch,r; 
+  printf("\nenter the limit : ");
+  scanf("%d",&ch);
+  for(i=0;i<ch;i++)
+  { r=rand();
+    a[i]=r;
+  }
+  t1=clock();
+  printf("\nposition : ");
+  scanf("%d",&i);
+  m=--i;
+  for(;i<ch-1;i++)
+   a[i]=a[i+1];
+  
+  
+  lar=a[m++];
+  for(;m<n-1;m++)
+   if(a[m]>lar)
+    lar=a[m];
+  printf("  the next  Largest element is  : %d\n",lar);
+  t2=clock();
+  double m1;
+  m1=(t2-t1)/(double)CLOCKS_PER_SEC;
+  printf("Time taken is  : %f\n",m1);
+}
---
+++ ll.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+clock_t t1,t2;
+struct node
+{ int d;
+  struct node *next; };
+
+void main()
+{ int ch,it,k,n,i=1;
+  struct node *h,*temp,*l,*b,*p,*lar;
+  h=NULL;
+printf("enter the limit :");
+scanf("%d",&k);
+  do{
+    temp=(struct node *)malloc(sizeof(struct node));
+    temp->d=rand();
+    temp->next=NULL;
+    if(h==NULL)
+    { h=temp;
+      l=temp;
+    }
+    else
+    { l->next=temp;
+      l=temp;
+    }
+    i++;
+  }while(i<1000);
+ 
+
+  t1=clock();
+  printf("position :");
+  scanf("%d",&n);
+  if(h->d==n)
+  { temp=h;
+    h=h->next;
+    free(temp);
+  } 
+
+  l=h;
+  for(i=0;i<k;i++)
+  { if(i==n)
+    { b->next=l->next;
+      lar=l->next;
+      it=l->next->d;
+      free(l);
+    }
+    else
+    { b=l;
+      l=l->next;
+    }
+  }
+ 
+  while(lar!=NULL)
+  { if(lar->d>it)
+     it=lar->d;
+    lar=lar->next;
+  }
+  printf("   the next Largest element is  : %d",it);
+  t2=clock();
+  double m1;
+  m1=(t2-t1)/(double)CLOCKS_PER_SEC;
+  printf("\nTime taken is  : %f\n",m1);
+}
+  
+  
+
+
+
+
+
+
